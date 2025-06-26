6bd5245ac9ccb580836b84e9e6f9b823955cfd68
Success12016 <pattarachai12016@gmail.com>
2025-06-27 02:53:14 +0700

    done simple

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const express = require("express");
+const bodyparser = require("body-parser");
+
+const app = express();
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.get("/" , function(req , res){
+    res.sendFile(__dirname + "/");
+});
+
+app.post("/" , function (req , res){
+    const body = req.body;
+    const weight = parseFloat(body.weight);
+    const height = parseFloat(body.height)/100;
+    const result = weight / (height * height);
+    res.send('Your BMI result is ' + result);
+});
+
+app.listen(3000 , function(){
+    console.log("Server is running on port 3000.");
+});
